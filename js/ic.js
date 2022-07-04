45c91958c3ba5be8d158aa0ecd815f275f015851
eiji-shimizu <eijishimizu9@gmail.com>
2022-07-04 17:37:38 +0900

    ホ長調を追加

--- js/ic.js
+++ js/ic.js
@@ -1,57 +1,94 @@
 const chordNames = [
-    "C", "Cm", "Cm-5",
-    "C#", "C#m", "C#m-5",
-    "Cb", "Cbm", "Cbm-5",
-    "D", "Dm", "Dm-5",
-    "D#", "D#m", "D#m-5",
-    "Db", "Dbm", "Dbm-5",
-    "E", "Em", "Em-5",
-    "E#", "E#m", "E#m-5",
-    "Eb", "Ebm", "Ebm-5",
-    "F", "Fm", "Fm-5",
-    "F#", "F#m", "F#m-5",
-    "Fb", "Fbm", "Fbm-5",
-    "G", "Gm", "Gm-5",
-    "G#", "G#m", "G#m-5",
-    "Gb", "Gbm", "Gbm-5",
-    "A", "Am", "Am-5",
-    "A#", "A#m", "A#m-5",
-    "Ab", "Abm", "Abm-5",
-    "B", "Bm", "Bm-5",
-    "B#", "B#m", "B#m-5",
-    "Bb", "Bbm", "Bbm-5",
+    "C", "Cm", "Cmb5",
+    "Cs", "Csm", "Csmb5",
+    "Cb", "Cbm", "Cbmb5",
+    "D", "Dm", "Dmb5",
+    "Ds", "Dsm", "Dsmb5",
+    "Db", "Dbm", "Dbmb5",
+    "E", "Em", "Emb5",
+    "Es", "Esm", "Esmb5",
+    "Eb", "Ebm", "Ebmb5",
+    "F", "Fm", "Fmb5",
+    "Fs", "Fsm", "Fsmb5",
+    "Fb", "Fbm", "Fbmb5",
+    "G", "Gm", "Gmb5",
+    "Gs", "Gsm", "Gsmb5",
+    "Gb", "Gbm", "Gbmb5",
+    "A", "Am", "Amb5",
+    "As", "Asm", "Asmb5",
+    "Ab", "Abm", "Abmb5",
+    "B", "Bm", "Bmb5",
+    "Bs", "Bsm", "Bsmb5",
+    "Bb", "Bbm", "Bbmb5",
 ];
 
 const chordMap = {
     // C
+    //C: ["C3"],
     C: ["C3", "E3", "G3", "C4", "E4"],
     Cm: ["C3", "G3", "C4", "Eb4", "G4"],
-    // C#
+    // Cs
+    Cs: ["C#3", "F3", "G#3", "C#4", "F4"],
+    Csm: ["C#3", "G#3", "C#4", "E4", "G#4"],
     // Cb
+    Cb: [],
+    Cbm: [],
     // D
     D: ["D3", "A3", "D4", "F#4"],
     Dm: ["D3", "A3", "D4", "F4"],
-    // D#
+    // Ds
+    Ds: ["D#3", "A#3", "D#4", "G4"],
+    Dsm: ["D#3", "A#3", "D#4", "F#4"],
+    Dsmb5: ["D#3", "A3", "D#4", "F#4"],
     // Db
+    Db: [],
+    Dbm: [],
     // E
-    // E#
+    E: ["E2", "B2", "E3", "G#3", "B3", "E4"],
+    Em: ["E2", "B2", "E3", "G3", "B3", "E4"],
+    // Es
+    Es: [],
+    Esm: [],
     // Eb
+    Eb: [],
+    Ebm: [],
     // F
     F: ["F2", "C3", "F3", "A3", "C4", "F4"],
     Fm: ["F2", "C3", "F3", "Ab3", "C4", "F4"],
-    // F#
+    // Fs
+    Fs: ["F#2", "C#3", "F#3", "A#3", "C#4", "F#4"],
+    Fsm: ["F#2", "C#3", "F#3", "A3", "C#4", "F#4"],
     // Fb
+    Fb: [],
+    Fbm: [],
     // G
     G: ["G2", "B2", "D3", "G3", "B3", "G4"],
     Gm: ["G2", "Bb2", "D3", "G3", "D4", "G4"],
-    // G#
+    // Gs
+    Gs: ["G#2", "D#3", "G#3", "C4", "D#4", "G#4"],
+    Gsm: ["G#2", "D#3", "G#3", "B3", "D#4", "G#4"],
     // Gb
+    Gb: [],
+    Gbm: [],
     // A
-    // A#
+    A: ["A2", "E3", "A3", "C#4", "E4"],
+    Am: ["A2", "E3", "A3", "C4", "E4"],
+    // As
+    As: [],
+    Asm: [],
     // Ab
+    Ab: [],
+    Abm: [],
     // B
-    // B#
+    B: ["B2", "F#3", "B3", "D#4", "F#4"],
+    Bm: ["B2", "F#3", "B3", "D4", "F#4"],
+    Bmb5: ["B2", "D3", "B3", "D4", "F4"],
+    // Bs
+    Bs: [],
+    Bsm: [],
     // Bb
+    Bb: [],
+    Bbm: [],
 };
 
 window.addEventListener("load", () => {
